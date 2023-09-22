13510859de2f177c9bc933418503a431ece9432c
asalehe <126200235+alirezasalehe@users.noreply.github.com>
2023-09-22 22:16:14 +0330

    Improve quality

--- main.py
+++ main.py
@@ -1,54 +1,37 @@
 NUMBERS = "1234567890"
-OPERATIONS = "x/+-"
+OPERATORS = "x/+-"  # In order of priority
 
 
 def calculate(expression):
-    result = expression
-    while have_strings_any_common_characters(expression, OPERATIONS):
-        highest_priority_expression = get_higest_priority_operation(expression)
+    while have_strings_any_common_characters(expression, OPERATORS):
+        highest_priority_expression = get_expression_around_operator(expression,
+                                                                     get_highest_priority_operator_index(expression))
         result = calculate_simple_expression(highest_priority_expression)
         expression = expression.replace(highest_priority_expression, str(result))
-    return result
+    return expression
 
 
-def get_higest_priority_operation(expression):
-    index = len(expression)
-    flag = False
-    if "x" in expression:
-        index = expression.index("x")
-        flag = True
-    if "/" in expression:
-        substraction_index = expression.index("/")
-        if substraction_index < index:
-            return get_expression_around(substraction_index, expression)
-        if flag:
-            return get_expression_around(index, expression)
-
-    if "+" in expression:
-        index = expression.index("+")
-        flag = True
-    if "-" in expression:
-        substraction_index = expression.index("-")
-        if substraction_index < index:
-            return get_expression_around(substraction_index, expression)
-        if flag:
-            return get_expression_around(index, expression)
+def get_highest_priority_operator_index(expression):
+    for operator in OPERATORS:
+        if operator in expression:
+            return expression.index(operator)
 
 
-def get_expression_around(index, expression):
-    start_index = find_operation_previous_number_start_index(index, expression)
-    end_index = find_operation_next_number_end_index(index, expression)
-    return expression[start_index, end_index]
+def get_expression_around_operator(expression, index):
+    start = find_previous_number_start_index(expression, index)
+    end = find_next_number_end_index(expression, index)
+
+    return expression[start:end]
 
 
-def find_operation_previous_number_start_index(index, expression):
+def find_previous_number_start_index(expression, index):
     pointer = index - 1
-    while pointer >= 0 and expression[pointer] in NUMBERS:
+    while pointer > 0 and expression[pointer] in NUMBERS:
         pointer -= 1
     return pointer
 
 
-def find_operation_next_number_end_index(index, expression):
+def find_next_number_end_index(expression, index):
     pointer = index + 1
     while pointer < len(expression) and expression[pointer] in NUMBERS:
         pointer += 1
@@ -63,11 +46,11 @@ def have_strings_any_common_characters(string1, string2):
 
 
 def calculate_simple_expression(simple_expression):
+    # TODO
     return 0
 
 
 done = False
-
 while not done:
     user_input = input(">>> ")
     if user_input == "exit":
